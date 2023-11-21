c237dd517befd118ca9ea426ae46e89745a22eed
CodeSandbox <support@codesandbox.io>
2023-11-21 18:25:44 +0000

    Initial commit

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import psycopg2
 from flask_cors import CORS
+
 app = Flask(__name__)
 CORS(app)
+
 # Define your database connection parameters
 db_params = {
     "dbname": "postgres",
@@ -38,6 +40,30 @@ def get_data():
     except Exception as e:
         return jsonify({"error": str(e)})
 
+
+@app.route('/assign', methods=['POST'])
+def assign_task():
+    try:
+        data = request.json
+        task_id = data.get("task_id")
+        assignee = data.get("assignee")
+
+        # Establish a connection to the PostgreSQL database
+        conn = psycopg2.connect(**db_params)
+        cursor = conn.cursor()
+
+        # Update the database with the assigned assignee for the specified task_id
+        query = "UPDATE email_table SET assignee = %s WHERE task_id = %s"
+        cursor.execute(query, (assignee, task_id))
+        conn.commit()
+
+        # Close the cursor and connection
+        cursor.close()
+        conn.close()
+
+        return jsonify({"message": "Task assigned successfully"})
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
 if __name__ == '__main__':
     app.run(debug=True)
