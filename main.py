7b917acf52c80fb2ef1ff043c8fc6caa672f2237
CodeSandbox <support@codesandbox.io>
2023-11-21 15:57:48 +0000

    Sandbox snapshot commit

--- main.py
+++ main.py
@@ -1,12 +1,42 @@
-from flask import Flask
+from flask import Flask, jsonify
+import psycopg2
 
 app = Flask(__name__)
 
+# Define your database connection parameters
+db_params = {
+    "dbname": "name",
+    "user": "name",
+    "password": "name",
+    "host": "googl.com",
+    "port": "0000"
+}
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+
+@app.route('/api/get_data', methods=['GET'])
+def get_data():
+    try:
+        # Establish a connection to the PostgreSQL database
+        conn = psycopg2.connect(**db_params)
+        cursor = conn.cursor()
+
+        # Execute a query to retrieve data (modify the query as needed)
+        query = "SELECT * FROM email_table"
+        cursor.execute(query)
+        data = cursor.fetchall()
+
+        # Convert the data to a list of dictionaries
+        columns = [desc[0] for desc in cursor.description]
+        result = [dict(zip(columns, row)) for row in data]
+
+        # Close the cursor and connection
+        cursor.close()
+        conn.close()
+
+        return jsonify(result)
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
